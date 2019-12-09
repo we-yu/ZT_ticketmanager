eb863308a945e594c74aa8fc2fbf79798e95af6d
we-yu <whiterythronium@gmail.com>
2019-12-10 02:05:05 +0700

    Formatted done, from spreadsheet ticket information

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 var CATCHER = "@taskbot"
 
+function sandBox() {
+  var txt;
+  txt = "abcdefghijklmnopqrstuvwxyz";
+  Logger.log(txt);
+  Logger.log(txt.slice(0, 10));
+}
+
 function isBotMessage(tgtTxt) {
   return (tgtTxt[0] === CATCHER) ? true : false;
 }
 
+function padString(tgtText, width, m) {
+  padWidth = width - 1;
+  width = width * -1;
+  padding = "";
+  for (var i = 0 ; i < padWidth ; i++ ){
+    padding = padding + m;
+  }
+
+  padTxt = (padding + tgtText).substr(width);
+  return padTxt;
+}
+
+/**
+ 右埋めする処理
+ 指定桁数になるまで対象文字列の右側に
+ 指定された文字を埋めます。
+ @param val 右埋め対象文字列
+ @param char 埋める文字
+ @param n 指定桁数
+ @return 右埋めした文字列
+**/
+function paddingright(val,char,n){
+  for(; val.length < n; val+=char);
+  return val;
+ }
+ 
 function textDivide(tgtText) {
   bodyText = tgtText;
   toBotText = bodyText.replace(/(,|\.)/gi, ' ');  // , and . replace to blank
@@ -13,9 +46,9 @@ function textDivide(tgtText) {
   botTextArray = toBotText.split(" ");
   reText = botTextArray.join(",");
 
-  debugLog("Original TEXT :" + bodyText);
-  debugLog("Replaced TEXT :" + toBotText);
-  debugLog("Rebuild  TEXT :" + reText);
+  // debugLog("Original TEXT :" + bodyText);
+  // debugLog("Replaced TEXT :" + toBotText);
+  // debugLog("Rebuild  TEXT :" + reText);
 
   return botTextArray;
 }
@@ -37,16 +70,13 @@ function doPost(e) {
 
   var roomId = json.webhook_event.room_id;
   url = "https://api.chatwork.com/v2/rooms/" + roomId + "/messages";
-  debugLog(url);
-  debugLog(json);
-  debugLog("Catcher = " + CATCHER);
+  // debugLog(url);
+  // debugLog(json);
+  // debugLog("Catcher = " + CATCHER);
   
   var bodyText = json.webhook_event.body;
 
   var dividedTxt = textDivide(bodyText);
-  for (var i = 0 ; i < dividedTxt.length ; i++ ) {
-    debugLog(i + " : " + dividedTxt[i]);
-  }
 
   if (isBotMessage(dividedTxt) === false) {
     debugLog("a This is NOT bot message : " + bodyText);
@@ -58,17 +88,15 @@ function doPost(e) {
   }
 
   var cmd = getCommandCode(dividedTxt);
-  debugLog("cmd == ", cmd, typeof cmd);
+  // debugLog("cmd == ", cmd, typeof cmd);
   var match = "read";
 
-  debugLog(cmd, ":", match);
-  debugLog(typeof cmd, ":", typeof match);
+  // debugLog(cmd, ":", match);
+  // debugLog(typeof cmd, ":", typeof match);
 
 
   var tgtFile  = SpreadsheetApp.getActiveSpreadsheet();
   var tgtSheet = tgtFile.getActiveSheet();
-  debugLog(tgtFile.getName());
-  debugLog(tgtSheet.getName());
 
   var startrow = 1;
   var startcol = 1;
@@ -77,21 +105,31 @@ function doPost(e) {
   
   var sheetdata = tgtSheet.getSheetValues(startrow, startcol, lastrow, lastcol);
 
+  var textWidths = [6, 14, 10, 12, 12, 18, 42];
+
   msgs = [];
   rowdata = "";
   for (var i = 0 ; i < lastrow ; i++ ){
       for (var j = 0 ; j < lastcol ; j++ ){
-        rowdata += sheetdata[i][j];
-        rowdata += " : ";
+        if (j == 7) break;
+        item  = sheetdata[i][j];
+        if(i != 0 && j == 5){
+          item = Utilities.formatDate(item, 'Asia/Bangkok', 'yyyy-MM-dd HH:mm');
+        }
+        if(i != 0 && j == 6){
+          item = item.slice(0, 40);
+        }
+        item  = item.toString();
+        item  = paddingright(item, " ", textWidths[j]);
+
+        rowdata += item;
       }
     msgs.push(rowdata);
     rowdata = "";
   }
-  for (var i = 0 ; i < msgs.length ; i++ ){
-    debugLog(msgs[i]);
-  }
-
-
+  // for (var i = 0 ; i < msgs.length ; i++ ){
+  //   debugLog(msgs[i]);
+  // }
   // -------------------------
   
   var accountId = json.webhook_event.account_id;
@@ -108,6 +146,4 @@ function doPost(e) {
   params.payload = {body :body};
   
   UrlFetchApp.fetch(url, params);
-  
 }
-
